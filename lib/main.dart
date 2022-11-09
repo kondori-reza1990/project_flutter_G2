2a25a847dfb6908afcf6cdb82f3e334e6a849cd0
Alipour <kondori.reza1990@gmail.com>
2022-11-09 18:13:42 +0330

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(SecondPage());
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+
+  String text = "My First Project in FLUTTER";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Column(
+              children: [
+                FlutterLogo(
+                  size: 250,
+                ),
+                Text(
+                    "Title",
+                  style: TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(15),
+                  child: Text("The Flutter logo, in widget form. This widget respects the IconTheme. For guidelines on using the Flutter logo, visit https://flutter.dev/brand. See also: IconTheme, which provides ambient configuration for icons. Icon, for showing icons the Material design icon library. ImageIcon, for showing icons from AssetImages or other ImageProviders.",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () { },
+                    child: Text("Save")
+                )
+              ],
+            ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
