47c382142a6670f12aab35e428d1691c57a80833
Alipour <kondori.reza1990@gmail.com>
2022-12-05 17:05:24 +0330

    change code in 05-15-2022

--- lib/add_card.dart
+++ lib/add_card.dart
@@ -13,14 +13,79 @@ class AddCard extends StatefulWidget {
 class _AddCardState extends State<AddCard> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
         children: [
-          Image.asset("assets/images/update.png"),
-          TextField(
-            decoration: InputDecoration(
-              label: Text("Card Id")
+          Container(
+            color: Colors.indigo,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: h * 3 / 4,
+              padding: EdgeInsets.fromLTRB(20, 90, 20, 20),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(50)
+                )
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Card Number",
+                        prefixIcon: Icon(Icons.credit_card),
+                      ),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              labelText: "Expiry Date",
+                              prefixIcon: Icon(Icons.date_range),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                labelText: "Cvv2",
+                                prefixIcon: Icon(Icons.numbers),
+                              ),
+                            )
+                        ),
+                      ],
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Name",
+                        prefixIcon: Icon(Icons.person),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
+          ),
+          Positioned(
+            left: w / 5,
+              top: h / 4 - 75,
+              child: Container(
+                width: w * 3 / 5,
+                height: 150,
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(40)
+                ),
+              )
           )
         ],
       )
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
+import 'package:demo_project/add_card.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const SecondPage());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SecondPage(),
+    );
+  }
+}
+
 
 class SecondPage extends StatefulWidget {
   const SecondPage({Key? key}) : super(key: key);
@@ -15,39 +29,45 @@ class _SecondPageState extends State<SecondPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              Image.asset(
-              "assets/images/image1.png"
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset(
+                "assets/images/image1.png"
+            ),
+            Text(
+              "Title",
+              style: TextStyle(
+                  fontSize: fontSize,
+                  fontFamily: "titleFont",
+                  fontWeight: FontWeight.bold
               ),
-              Text(
-                "Title",
-                style: TextStyle(
-                    fontSize: fontSize,
-                    fontFamily: "titleFont",
-                    fontWeight: FontWeight.bold
-                ),
+            ),
+            Expanded(
+              child: Container(
+                  margin: EdgeInsets.all(15),
+                  child: const SingleChildScrollView(
+                    child: Text(
+                      "The Flutter logo, in widget form. This widget respects the IconTheme. For guidelines on using the Flutter logo, visit https://flutter.dev/brand. See also: IconTheme, which provides ambient configuration for icons. Icon, for showing icons the Material design icon library. ImageIcon, for showing icons from AssetImages or other ImageProviders. The Flutter logo, in widget form. This widget respects the IconTheme. For guidelines on using the Flutter logo, visit https://flutter.dev/brand. See also: IconTheme, which provides ambient configuration for icons. Icon, for showing icons the Material design icon library. ImageIcon, for showing icons from AssetImages or other ImageProviders.",
+                      style: TextStyle(fontSize: 20, color: Colors.grey),
+                    ),
+                  )
               ),
-              Expanded(
-                  child: Container(
-                      margin: EdgeInsets.all(15),
-                      child: SingleChildScrollView(
-                        child: Text(
-                          "The Flutter logo, in widget form. This widget respects the IconTheme. For guidelines on using the Flutter logo, visit https://flutter.dev/brand. See also: IconTheme, which provides ambient configuration for icons. Icon, for showing icons the Material design icon library. ImageIcon, for showing icons from AssetImages or other ImageProviders. The Flutter logo, in widget form. This widget respects the IconTheme. For guidelines on using the Flutter logo, visit https://flutter.dev/brand. See also: IconTheme, which provides ambient configuration for icons. Icon, for showing icons the Material design icon library. ImageIcon, for showing icons from AssetImages or other ImageProviders.",
-                          style: TextStyle(fontSize: 20, color: Colors.grey),
-                        ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AddCard()
                       )
-                  ),
-              ),
-              ElevatedButton(onPressed: () {}, child: Text("Save"))
-            ],
-          ),
+                  );
+                },
+                child: Text("Add Card"))
+          ],
         ),
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
